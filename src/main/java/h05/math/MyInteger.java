3c12353bed5ebb808a9a7652ed806df1f0766246
Oshgnacknak <osh@oshgnacknak.de>
2022-06-05 18:27:34 +0200

    Make argument of MyInteger.times not nullable

--- src/main/java/h05/math/MyInteger.java
+++ src/main/java/h05/math/MyInteger.java
@@ -2,7 +2,6 @@ package h05.math;
 
 import h05.exception.Comparison;
 import h05.exception.WrongOperandException;
-import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -105,7 +104,7 @@ public final class MyInteger extends MyNumber {
     }
 
     @Override
-    public MyNumber times(@Nullable MyNumber other) {
+    public MyNumber times(MyNumber other) {
         if (other instanceof MyInteger) {
             return new MyInteger(value.multiply(other.toInteger()));
         }
