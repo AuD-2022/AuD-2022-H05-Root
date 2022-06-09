fb4d15f3db2712bedec362d56f91eda8c01a6dd4
Oshgnacknak <osh@oshgnacknak.de>
2022-06-09 22:26:43 +0200

    Add public tests for MyInteger

--- src/test/java/h05/PublicTests.java
+++ src/test/java/h05/PublicTests.java
@@ -6,7 +6,6 @@ import h05.math.MyReal;
 import h05.math.Rational;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.opentest4j.AssertionFailedError;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -15,26 +14,107 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PublicTests {
 
-    private static final BigDecimal EPSILON = new BigDecimal("0.0000000000001");
+    private static final BigDecimal EPSILON = real("0.0000000000001");
 
     @Nested
     class RationalTest {
 
         @Test
         void testConstructor() {
-            var minusTwoThirds = new Rational(
-                BigInteger.valueOf(10),
-                BigInteger.valueOf(-15));
+            var minusTwoThirds = ratio(10, -15);
             assertEquals(BigInteger.valueOf(-2), minusTwoThirds.getNumerator());
             assertEquals(BigInteger.valueOf(3), minusTwoThirds.getDenominator());
         }
     }
 
     @Nested
+    class MyIntegerTest {
+
+        private static final MyInteger sixtyNine = mint(69);
+
+        @Test
+        void testToRational() {
+            assertEquals(
+                mratio(69, 1).toRational(),
+                sixtyNine.toRational());
+        }
+
+        @Test
+        void testToReal() {
+            assertEquals(
+                real("69"),
+                sixtyNine.toReal());
+        }
+
+        @Test
+        void testMinus() {
+            assertEquals(
+                mint(-69),
+                sixtyNine.minus());
+        }
+
+        @Test
+        void testMinusWithOperand() {
+            assertEquals(
+                mreal("55.63"),
+                sixtyNine.minus(mreal("13.37")));
+        }
+
+        @Test
+        void testDivide() {
+            assertEquals(
+                mratio(1, 69),
+                sixtyNine.divide());
+        }
+
+        @Test
+        void testDivideWithOperand() {
+            assertEquals(
+                mint(23),
+                sixtyNine.divide(mint(3)));
+        }
+
+        @Test
+        void testSqrt() {
+            assertEquals(
+                mint(4),
+                mint(16).sqrt());
+        }
+
+        @Test
+        void testExpt() {
+            assertEquals(
+                mint(4761),
+                sixtyNine.expt(mint(2)));
+        }
+
+        @Test
+        void testExp() {
+            assertAlmostEquals(
+                real("54.59815003314423"),
+                mint(4).exp().toReal());
+        }
+
+        @Test
+        void testLn() {
+            assertAlmostEquals(
+                real("4.23410650459726"),
+                sixtyNine.ln().toReal());
+        }
+
+        @Test
+        void testLog() {
+            assertEquals(
+                mint(2),
+                mint(4761).log(sixtyNine));
+        }
+    }
+
+    @Nested
     class MyRationalTest {
 
-        private static final MyRational minusTwoThirds = ratio(-2, 3);
-        private static final MyRational four = ratio(4, 1);
+        private static final MyRational minusTwoThirds = mratio(-2, 3);
+        private static final MyRational threeHalf = mratio(3, 2);
 
         @Test
         void testToRational() {
@@ -46,93 +126,104 @@ public class PublicTests {
         @Test
         void testToReal() {
             assertAlmostEquals(
-                new BigDecimal("-0.666666666666667"),
+                real("-0.666666666666667"),
                 minusTwoThirds.toReal());
         }
 
         @Test
         void testMinus() {
             assertEquals(
-                ratio(2, 3),
+                mratio(2, 3),
                 minusTwoThirds.minus());
         }
 
         @Test
         void testMinusWithOperand() {
             assertEquals(
-                ratio(5, 3),
+                mratio(5, 3),
                 MyRational.ONE.minus(minusTwoThirds));
         }
 
         @Test
         void testDivide() {
             assertEquals(
-                ratio(-3, 2).toRational(),
+                mratio(-3, 2).toRational(),
                 minusTwoThirds.divide().toRational());
         }
 
         @Test
         void testDivideWithOperand() {
             assertEquals(
-                ratio(-8, 9),
-                minusTwoThirds.divide(ratio(3, 4)));
+                mratio(-8, 9),
+                minusTwoThirds.divide(mratio(3, 4)));
         }
 
         @Test
         void testSqrt() {
-            assertEquals(
-                integer(2),
-                four.sqrt());
+            assertAlmostEquals(
+                real("1.224744871391589"),
+                threeHalf.sqrt().toReal());
         }
 
         @Test
         void testExpt() {
             assertEquals(
-                integer(2),
-                four.expt(ratio(1, 2)));
+                mreal("2.25"),
+                threeHalf.expt(mint(2)));
         }
 
         @Test
         void testExp() {
             assertAlmostEquals(
-                real("54.59815003314423").toReal(),
-                four.exp().toReal());
+                real("4.4816890703380645"),
+                mratio(3, 2).exp().toReal());
         }
 
         @Test
         void testLn() {
             assertAlmostEquals(
-                real("1.3862943611198906").toReal(),
-                four.ln().toReal());
+                real("0.4054651081081644"),
+                threeHalf.ln().toReal());
         }
 
         @Test
         void testLog() {
-            assertAlmostEquals(
-                integer(-2).toReal(),
-                four.log(ratio(1, 2)).toReal());
+            assertEquals(
+                mint(-2),
+                mratio(1, 25).log(mint(5)));
         }
     }
 
 
-    private static MyInteger integer(int value) {
-        return new MyInteger(BigInteger.valueOf(value));
+    private static MyInteger mint(int value) {
+        return new MyInteger(integer(value));
     }
 
-    private static MyRational ratio(int numerator, int denominator) {
-        var ratio = new Rational(BigInteger.valueOf(numerator), BigInteger.valueOf(denominator));
-        return new MyRational(ratio);
+    private static BigInteger integer(int value) {
+        return BigInteger.valueOf(value);
     }
 
-    private static MyReal real(String value) {
-        return new MyReal(new BigDecimal(value));
+    private static MyRational mratio(int numerator, int denominator) {
+        return new MyRational(ratio(numerator, denominator));
+    }
+
+    private static Rational ratio(int numerator, int denominator) {
+        return new Rational(BigInteger.valueOf(numerator), BigInteger.valueOf(denominator));
+    }
+
+    private static MyReal mreal(String value) {
+        return new MyReal(real(value));
+    }
+
+    private static BigDecimal real(String value) {
+        return new BigDecimal(value).setScale(MyReal.SCALE, MyReal.ROUNDING_MODE);
     }
 
     private static void assertAlmostEquals(BigDecimal expected, BigDecimal actual) {
         var delta = expected.subtract(actual).abs();
 
         if (EPSILON.compareTo(delta) < 0) {
-            throw new AssertionFailedError("Given numbers are too far apart", expected, actual);
+            assertEquals(expected, actual);
         }
     }
 }
